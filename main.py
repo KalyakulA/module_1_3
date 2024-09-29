7ab4aa92fa691484b8521109a0921fffbd96cc5b
Андрей <kalyakula@gmail.com>
2024-09-29 20:51:33 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+name = 'Andrey'
+print ('Name:' , name)
+age = 38
+print('Age:' , age)
+new_age = 1
+print (('New Age:') , age + new_age)
+name = 'Is_Student'
+is_student = 'True'
+print('Is Student:' , is_student)
