9fe0ae247a81262ac2b1327f06f97680d55b9de4
KalyakulA <kalyakula@gmail.com>
2024-09-29 23:03:43 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-name = 'Andrey'
-print ('Name:' , name)
-age = 38
-print('Age:' , age)
-new_age = 1
-print (('New Age:') , age + new_age)
-name = 'Is_Student'
-is_student = 'True'
-print('Is Student:' , is_student)
+example = 'Шуруповерт'
+print(example [0])
+print(example [-1])
+print(example [5:])
+print(example [::-1])
+print(example [1::2])
