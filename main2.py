931a76bacc3878f9fcfbab183adfe80f4f0dd6f7
KalyakulA <kalyakula@gmail.com>
2024-09-29 22:28:25 +0300

    Add files via upload

---
+++ main2.py
@@ -0,0 +1,6 @@
+total_lessons = 12
+hours_spent = 1.5
+course_name = 'Python'
+time_one_lesson = hours_spent / total_lessons
+print('Курс:', course_name, ', всего задач:', total_lessons, ', затрачено часов:',
+hours_spent, ', среднее время выполнения -', time_one_lesson ,'часа.')
