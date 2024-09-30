d1b4cbd65ad3832f81dbe6052d85797f4c0d9b6f
KalyakulA <kalyakula@gmail.com>
2024-09-30 21:49:22 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ name = 'Andrey'
 print ('Name:' , name)
 age = 38
 print('Age:' , age)
-new_age = 1
-print (('New Age:') , age + new_age)
+age = age + 1
+print (('New Age:') , age)
 name = 'Is_Student'
 is_student = 'True'
 print('Is Student:' , is_student)
